e2be3b11e4ded8a54fa37160d1adb324559eddfd
Francisco <melicias1999@gmail.com>
2022-03-25 12:16:51 +0000

    added the template for the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,31 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+/*!
 
-import App from './App.vue'
-import router from './router'
+=========================================================
+* BootstrapVue Argon Dashboard - v1.0.0
+=========================================================
 
-const app = createApp(App)
+* Product Page: https://www.creative-tim.com/product/bootstrap-vue-argon-dashboard
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
 
-app.use(createPinia())
-app.use(router)
+* Coded by www.creative-tim.com
 
-app.mount('#app')
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Vue from 'vue';
+import DashboardPlugin from './plugins/dashboard-plugin';
+import App from './App.vue';
+
+// router setup
+import router from './routes/router';
+// plugin setup
+Vue.use(DashboardPlugin);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+});
