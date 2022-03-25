749c688ba473ca2708663ba5aca55ad736c5734e
Daniel Sarreira <2191740@my.ipleiria.pt>
2022-03-25 12:39:07 +0000

    Revert "added the template for the project"
    This reverts commit e2be3b11e4ded8a54fa37160d1adb324559eddfd.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,31 +1,12 @@
-/*!
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-=========================================================
-* BootstrapVue Argon Dashboard - v1.0.0
-=========================================================
+import App from './App.vue'
+import router from './router'
 
-* Product Page: https://www.creative-tim.com/product/bootstrap-vue-argon-dashboard
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+const app = createApp(App)
 
-* Coded by www.creative-tim.com
+app.use(createPinia())
+app.use(router)
 
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import Vue from 'vue';
-import DashboardPlugin from './plugins/dashboard-plugin';
-import App from './App.vue';
-
-// router setup
-import router from './routes/router';
-// plugin setup
-Vue.use(DashboardPlugin);
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: h => h(App),
-  router
-});
+app.mount('#app')
