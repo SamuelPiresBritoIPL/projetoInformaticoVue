2599b446f64ac363cf99f3ef22c7fcccf967a580
Daniel Sarreira <2191740@my.ipleiria.pt>
2022-03-28 23:07:01 +0100

    Plugins

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,26 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import 'bootstrap/dist/js/bootstrap.bundle';
-import "bootstrap"
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(bootstrap)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+
 
 app.mount('#app')
