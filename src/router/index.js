b19193923d35dba9f12b6acd6fe5ec9b3b25c611
Francisco <melicias1999@gmail.com>
2022-05-20 10:34:57 +0100

    c

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ import GerirCadeiraProf from '../components/professor/gerircadeira.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/adminlogin',
