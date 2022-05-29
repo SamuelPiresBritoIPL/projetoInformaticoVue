4e16a843301553dcefd45e144890f71131ae13f6
Daniel Sarreira <2191740@my.ipleiria.pt>
2022-05-29 20:04:50 +0100

    cols

--- src/components/aluno/inscricaoturnos.vue
+++ src/components/aluno/inscricaoturnos.vue
@@ -47,13 +47,13 @@
                   <div v-if="buttonArray[index]" style="margin-top: 15px; text-align: left; margin-bottom: 35px;">
                     <div v-for="(inscricaoucs, index2) in cadeirasWithTurnosPorCurso" :key="inscricaoucs.id">
                       <div v-if="index == index2">
-                        <label class="col-sm-4 col-form-label"><strong>Unidade Curricular </strong>(código/nome)</label>   
-                        <label class="col-sm-8 col-form-label"><strong>Turnos diponíveis </strong>(inscritos/vagas)</label>
+                        <label class="col-5 col-form-label"><strong>Unidade Curricular </strong>(código/nome)</label>   
+                        <label class="col-7 col-form-label"><strong>Turnos diponíveis </strong>(inscritos/vagas)</label>
                         <br>
                         <div v-for="(cadeira, cadeiraIndex) in inscricaoucs" :key="cadeira.cadeira.id">
                           <div v-if="cadeira.cadeira.turnos.length != 0">
-                            <label class="col-sm-4 col-form-label" style="vertical-align: middle; float: left;">{{ "["+cadeira.cadeira.codigo+"] "+cadeira.cadeira.nome }}</label>   
-                            <label class="col-sm-8 col-form-label">
+                            <label class="col-5 col-form-label" style="vertical-align: middle; float: left;">{{ "["+cadeira.cadeira.codigo+"] "+cadeira.cadeira.nome }}</label>   
+                            <label class="col-7 col-form-label">
                               <span v-for="(turno, index) in cadeira.cadeira.turnos" :key="turno" style="margin-right: 20px;">
                                 <span style="margin-left: 10px;" v-for="(turnotipo) in turno" :key="turnotipo.id">
                                   <input class="form-check-input" type="radio" :value="turnotipo.id" v-model="arrayVmodel[cadeiraIndex][index]" style="margin-right: 3px" @click="clearRadio(cadeiraIndex, index, turnotipo.id)">
