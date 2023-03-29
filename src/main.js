b0db69730e4f022d36aba50135188cb82f898a6c
Samuel Pires Brito <2200653@my.ipleiria.pt>
2023-03-29 21:52:23 +0100

    Changed formating in some files (does the same)

--- src/main.js
+++ src/main.js
@@ -1,56 +1,53 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 import axios from "axios";
 
-import VueSocketIO from 'vue-3-socket.io'
+import VueSocketIO from "vue-3-socket.io";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
 import bootstrap from "bootstrap/dist/css/bootstrap.css";
 import bootstrapJS from "bootstrap/dist/js/bootstrap.js";
 import boostrapIcons from "bootstrap-icons/font/bootstrap-icons.css";
-import vueselect from 'vue-select/dist/vue-select.css';
-import BootstrapIcon from '@dvuckovic/vue3-bootstrap-icons';
+import vueselect from "vue-select/dist/vue-select.css";
+import BootstrapIcon from "@dvuckovic/vue3-bootstrap-icons";
 
 import Toaster from "@meforma/vue-toaster";
 
-import vSelect from 'vue-select'
-
+import vSelect from "vue-select";
 
 let toastOptions = {
-  position: "top",
-  timeout: 3000,
-  pauseOnHover: true,
-  queue: true,
-  dismissible: true,
+	position: "top",
+	timeout: 3000,
+	pauseOnHover: true,
+	queue: true,
+	dismissible: true,
 };
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import 'gitart-vue-dialog/dist/style.css'
-import { GDialog } from 'gitart-vue-dialog'
-import { plugin as dialogPlugin } from 'gitart-vue-dialog'
-import VueCal from 'vue-cal'
-import vuecalptbr from 'vue-cal/dist/i18n/pt-br.es.js'
-import vuecalcss from 'vue-cal/dist/vuecal.css'
+import "gitart-vue-dialog/dist/style.css";
+import { GDialog } from "gitart-vue-dialog";
+import { plugin as dialogPlugin } from "gitart-vue-dialog";
+import VueCal from "vue-cal";
+import vuecalptbr from "vue-cal/dist/i18n/pt-br.es.js";
+import vuecalcss from "vue-cal/dist/vuecal.css";
 
+library.add(faUserSecret);
 
-library.add(faUserSecret)
-
-const app = createApp(App)
-
+const app = createApp(App);
 
 //O que é para usar localmente
-const apiDomain="http://127.0.0.1:8000"
-const wsConnection="http://localhost:8080"
+const apiDomain = "http://127.0.0.1:8000";
+const wsConnection = "http://localhost:8080";
 
 /*
 //O que está no servidor
@@ -65,31 +62,39 @@ const wsConnection = process.env.VUE_APP_WS_CONNECTION
 */
 
 const socketIO = new VueSocketIO({
-  debug: true,
-  connection: wsConnection,
-})
+	debug: true,
+	connection: wsConnection,
+});
 
-app.use(createPinia())
-app.use(router)
-app.use(bootstrap)
-app.use(bootstrapJS)
-app.use(boostrapIcons)
-app.use(vueselect)
-app.use(vuecalptbr)
-app.use(vuecalcss)
-app.component('BootstrapIcon', BootstrapIcon)
-app.component('v-select', vSelect)
-app.component('GDialog', GDialog)
-app.component('vue-cal', VueCal)
-app.use(dialogPlugin)
-app.use(Toaster, toastOptions)
-app.use(socketIO)
+app.use(createPinia());
+app.use(router);
+app.use(bootstrap);
+app.use(bootstrapJS);
+app.use(boostrapIcons);
+app.use(vueselect);
+app.use(vuecalptbr);
+app.use(vuecalcss);
+app.component("BootstrapIcon", BootstrapIcon);
+app.component("v-select", vSelect);
+app.component("GDialog", GDialog);
+app.component("vue-cal", VueCal);
+app.use(dialogPlugin);
+app.use(Toaster, toastOptions);
+app.use(socketIO);
 
 axios.defaults.baseURL = `${apiDomain}/api`;
 app.config.globalProperties.$serverUrl = apiDomain;
-axios.defaults.headers.common["Authorization"] = `Bearer ${sessionStorage.tokenAluno ? sessionStorage.tokenAluno : (sessionStorage.tokenAdmin ? sessionStorage.tokenAdmin : (sessionStorage.tokenCoordenador ? sessionStorage.tokenCoordenador : (sessionStorage.tokenProfessor ? sessionStorage.tokenProfessor : "")))
-  }`;
+axios.defaults.headers.common["Authorization"] = `Bearer ${
+	sessionStorage.tokenAluno
+		? sessionStorage.tokenAluno
+		: sessionStorage.tokenAdmin
+		? sessionStorage.tokenAdmin
+		: sessionStorage.tokenCoordenador
+		? sessionStorage.tokenCoordenador
+		: sessionStorage.tokenProfessor
+		? sessionStorage.tokenProfessor
+		: ""
+}`;
 app.config.globalProperties.$axios = axios;
 
-
-app.mount('#app')
+app.mount("#app");
